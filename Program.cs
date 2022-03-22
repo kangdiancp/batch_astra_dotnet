ad40ba999a4aba1b734889f7c072c62c6cb47300
kangdiancp <kangdiancp@gmail.com>
2022-03-22 13:59:45 +0700

    done

--- Program.cs
+++ Program.cs
@@ -48,7 +48,48 @@ Console.WriteLine($"Total Sum : {sum}");
 
 day03.Array2D.DisplayMatrix(day03.Array2D.RandomMatrix());*/
 
-day04.Collections.InitHashSet();
+//day04.Collections.InitHashSet();
+
+//----------------- quiz day04 -------//
+
+day04.Quiz.Soal01(new List<int> { 1, 2, 5, 6, 9 }, new List<int> { 1, 2, 3, 4 });
+
+
+List<int> c = new List<int> { 1, 2, 3, 4 };
+List<int> s02 = day04.Quiz.Soal02(ref c);
+//day04.Quiz.DisplayList(ref s02);
+
+List<string> str1 = new List<string> { "code", "bootcamp", "id", "net" };
+List<string> s03 = day04.Quiz.Soal02(ref str1);
+Console.WriteLine();
+//day04.Quiz.DisplayList(ref s03);
+
+//soal 2
+Dictionary<int,int> numbers1 = day04.Quiz.Soal06(new List<int>(){1,1,1,2,2,2,3,3,3,3,3});
+//day04.Quiz.DisplayDictionary(numbers1);
+
+
+
+//soal04
+List<int> e = new List<int> { 7, 2, 7, 1, 2, 5, 7, 1 };
+List<int> soal4 = day04.Quiz.Soal04(ref e);
+day04.Quiz.DisplayList(ref soal4);
+Console.WriteLine();
+//soal05
+List<string> arr1 = new List<string> { "Mangga", "Apel", "Melon", "Pisang", "Sirsak", "Tomat", "Nanas", "Nangka", "Timun", "Mangga" };
+List<string> arr2 = new List<string> { "Bayam", "Wortel", "Kangkung", "Mangga", "Tomat", "Kembang Kol", "Nangka", "Timun" };
+day04.Quiz.Soal05(arr1, arr2, out HashSet<string> same, out HashSet<string> different);
+
+Console.Write("same : ");
+day04.Quiz.DisplayHashSet(ref same);
+Console.WriteLine();
+Console.Write("different : ");
+day04.Quiz.DisplayHashSet(ref different);
+//soal 06
+var v6 = new List<int>() { 1, 1, 2, 2, 2, 3, 3, 4, 5, 6 };
+List<int> l6 = day04.Quiz.Soal03(ref v6);
+Console.WriteLine();
+//day04.Quiz.DisplayList(ref l6);
 
 Console.ReadLine();
 
---
+++ day04/Quiz.cs
@@ -0,0 +1,152 @@
+﻿namespace day04
+{
+    class Quiz
+    {
+        //soal 01 
+        public static void Soal01(List<int> a, List<int> b)
+        {
+            // input a : 1,2,5,6,9
+            //input b : 1,2,3,4
+            //output case1 irisan : 1,2
+            var s1 = new HashSet<int>(a);
+            s1.IntersectWith(b);
+            DisplayHashSet(ref s1);
+
+            var s2 = new HashSet<int>(a);
+            s2.UnionWith(b);
+            Console.WriteLine();
+            DisplayHashSet(ref s2);
+
+            var s3 = new HashSet<int>(a);
+            s3.ExceptWith(b);
+            Console.WriteLine();
+            DisplayHashSet(ref s3);
+
+        }
+        
+        //soal 02 reverse
+        public static List<T> Soal02<T>(ref List<T> inputs)
+        {
+            //declare variable for return value
+            List<T> outputs = new List<T>();
+
+            foreach (var item in inputs)
+            {
+                outputs.Add(item);
+            }
+
+            outputs.Reverse();
+
+            return outputs;
+        }
+
+        public static void DisplayList<T>(ref List<T> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write($" {item} ");
+            }
+        }
+
+        public static void DisplayHashSet<T>(ref HashSet<T> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write($" {item} ");
+            }
+        }
+
+        public static void DisplayDictionary(Dictionary<int,int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write($" {item.Key} = {item.Value} ");
+            }
+        }
+
+        //soal nomor 03
+        public static List<T> Soal03<T>(ref List<T> inputs)
+        {
+            List<T> result = new List<T>(inputs);
+            HashSet<T> numbers = new HashSet<T>();
+
+            foreach (var item in result)
+            {
+                numbers.Add(item);
+            }
+
+            return numbers.ToList();
+        }
+
+        //soal nomor 04
+        public static List<int> Soal04<T>(ref List<T> values)
+        {
+            List<int> longestSequence = new List<int>();
+
+            List<int> currentSequence = new List<int>();
+
+            int lastValue = int.MinValue;
+            // cast to List<int>
+            foreach (int currentValue in values.Cast<int>().ToList())
+            {
+                if (currentValue >= lastValue)
+                {
+                    lastValue = currentValue;
+                    currentSequence.Add(currentValue);
+                }
+                else
+                {
+                    // end of this sequence, start new sequence, check length,
+
+                    if (currentSequence.Count() >= longestSequence.Count())
+                    {
+                        longestSequence = currentSequence;
+                    }
+                    currentSequence = new List<int>();
+                    lastValue = currentValue;
+                    currentSequence.Add(currentValue);
+                }
+            }
+
+            // if last sequence element smaller than prvioud then do this
+            if (currentSequence.Count() >= longestSequence.Count())
+            {
+                longestSequence = currentSequence;
+            }
+
+            return longestSequence;
+
+
+        }
+
+        //soal nomor 05
+        public static void Soal05(List<string> arr1, List<string> arr2, out HashSet<string> same,
+            out HashSet<string> different)
+        {
+            same = new HashSet<string>(arr1);
+            different = new HashSet<string>(arr2);
+
+            same.IntersectWith(different);
+            different.SymmetricExceptWith(arr1);
+        }
+
+        //soal nomor 06
+        public static Dictionary<int,int>Soal06(List<int> numbers)
+        {
+            Dictionary<int,int> valueToCount = new Dictionary<int,int>();
+            numbers.ForEach(number =>
+            {
+                if (valueToCount.ContainsKey(number))
+                {
+                    valueToCount[number] = valueToCount[number] + 1;
+                }
+                else
+                {
+                    valueToCount.TryAdd(number, 1);
+                }
+                
+            });
+            return valueToCount;
+        }
+    }
+}
